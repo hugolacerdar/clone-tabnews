286bcd3ead3ab07f0be268fb64c3a5732dcf33bc
Hugo Lacerda <hugolacerdar@gmail.com>
2024-12-25 15:54:00 +0000

    feat: enhance status endpoint to include database version and connection metrics

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,33 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  await database.query("SELECT 1;");
-  return response.json({ status: "healthy" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const { server_version: serverVersion } = databaseVersionResult.rows[0];
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections",
+  );
+  const { max_connections: maxConnectionsValue } =
+    databaseMaxConnectionsResult.rows[0];
+
+  const databaseActiveConnectionsResult = await database.query(
+    "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = current_database();",
+  );
+  const { count: activeConnectionsValue } =
+    databaseActiveConnectionsResult.rows[0];
+
+  return response.json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: serverVersion,
+        max_connections: parseInt(maxConnectionsValue),
+        active_connections: activeConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;

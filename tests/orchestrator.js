5f4f32cdaca5c627739592975f5d4a91167b33c2
Hugo Lacerda <hugolacerdar@gmail.com>
2024-12-30 16:28:15 +0000

    feat: update test command for improved execution and add maxTimeout to retry logic

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,7 +5,7 @@ async function waitForAllServices() {
   await waitForWebServer();
 
   async function waitForWebServer() {
-    retry(fetchStatusPage, { retries: 100 });
+    await retry(fetchStatusPage, { retries: 100, maxTimeout: 1000 });
 
     async function fetchStatusPage() {
       const response = await fetch(`http://localhost:3000/api/v1/status`);

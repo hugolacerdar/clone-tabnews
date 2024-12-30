0fea8eb92146b7461f64f17cb7a4b9f77d9dd927
Hugo Lacerda <hugolacerdar@gmail.com>
2024-12-30 13:24:39 +0000

    feat: increase retry attempts for fetchStatusPage to improve service availability checks

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -5,7 +5,7 @@ async function waitForAllServices() {
   await waitForWebServer();
 
   async function waitForWebServer() {
-    retry(fetchStatusPage);
+    retry(fetchStatusPage, { retries: 100 });
 
     async function fetchStatusPage() {
       const response = await fetch(`http://localhost:3000/api/v1/status`);

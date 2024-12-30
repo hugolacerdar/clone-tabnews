28df69f3e4b5b8a98f9364655419324f318df768
Hugo Lacerda <hugolacerdar@gmail.com>
2024-12-30 13:06:44 +0000

    feat: add async-retry package and refactor tests to use orchestrator for service management

---
+++ tests/orchestrator.js
@@ -0,0 +1,23 @@
+import retry from "async-retry";
+import database from "infra/database";
+
+async function waitForAllServices() {
+  await waitForWebServer();
+
+  async function waitForWebServer() {
+    retry(fetchStatusPage);
+
+    async function fetchStatusPage() {
+      const response = await fetch(`http://localhost:3000/api/v1/status`);
+      if (response.status !== 200) {
+        throw new Error("Status page not ready");
+      }
+    }
+  }
+}
+
+async function cleanDatabase() {
+  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
+}
+
+export default { waitForAllServices, cleanDatabase };
--- tests/utils.js
+++
@@ -1,7 +0,0 @@
-import database from "infra/database";
-
-async function cleanDatabase() {
-  await database.query("DROP SCHEMA public CASCADE; CREATE SCHEMA public;");
-}
-
-export { cleanDatabase };

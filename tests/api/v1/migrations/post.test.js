e83ffd3ecda030504014cfbb0251ebbdb52ca7f9
Hugo Lacerda <hugolacerdar@gmail.com>
2025-01-05 14:23:03 +0000

    test: rename all descriptions to use new pattern

--- tests/api/v1/migrations/post.test.js
+++ tests/api/v1/migrations/post.test.js
@@ -5,29 +5,57 @@ beforeAll(async () => {
   await orchestrator.cleanDatabase();
 });
 
-test("GET /api/v1/migrations returns 200", async () => {
-  const response = await fetch(`http://localhost:3000/api/v1/migrations`, {
-    method: "POST",
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    describe("Running pending migrations", () => {
+      test("For the first time", async () => {
+        const response = await fetch(
+          `http://localhost:3000/api/v1/migrations`,
+          {
+            method: "POST",
+          },
+        );
+
+        expect(response.status).toBe(201);
+
+        const responseBody = await response.json();
+
+        expect(Array.isArray(responseBody)).toBe(true);
+        expect(responseBody.length).toBeGreaterThan(0);
+
+        const secondResponse = await fetch(
+          `http://localhost:3000/api/v1/migrations`,
+          {
+            method: "POST",
+          },
+        );
+
+        expect(secondResponse.status).toBe(200);
+
+        const secondResponseBody = await secondResponse.json();
+
+        expect(Array.isArray(secondResponseBody)).toBe(true);
+        expect(secondResponseBody.length).toBe(0);
+      });
+      test("Before the first time", async () => {
+        await fetch(`http://localhost:3000/api/v1/migrations`, {
+          method: "POST",
+        });
+
+        const secondTimeResponse = await fetch(
+          `http://localhost:3000/api/v1/migrations`,
+          {
+            method: "POST",
+          },
+        );
+
+        expect(secondTimeResponse.status).toBe(200);
+
+        const secondTimeResponseBody = await secondTimeResponse.json();
+
+        expect(Array.isArray(secondTimeResponseBody)).toBe(true);
+        expect(secondTimeResponseBody.length).toBe(0);
+      });
+    });
   });
-
-  expect(response.status).toBe(201);
-
-  const responseBody = await response.json();
-
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
-
-  const secondResponse = await fetch(
-    `http://localhost:3000/api/v1/migrations`,
-    {
-      method: "POST",
-    },
-  );
-
-  expect(secondResponse.status).toBe(200);
-
-  const secondResponseBody = await secondResponse.json();
-
-  expect(Array.isArray(secondResponseBody)).toBe(true);
-  expect(secondResponseBody.length).toBe(0);
 });

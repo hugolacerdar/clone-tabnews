7b3bdd3d322e0a4a1c37c12f3c579b968529d872
Hugo Lacerda <hugolacerdar@gmail.com>
2025-01-05 20:10:57 -0300

    General refactoring (#23)
    * refactor: rename `wait-for-postgres-script` to `services:wait:database`
    
    * refactor: rename migration script to use `migrations` instead of `migration`
    
    * refactor: move tests scripts to top
    
    * feat: add script to manage development environment and cleanup on exit
    
    * feat: add posttest script to stop services after tests
    
    * chore: enhance test:watch script with verbose output
    
    * test: rename all descriptions to use new pattern
    
    * build: update node-pg-migrate to version 7.6.1 in package.json and package-lock.json
    
    * fix: replace join with resolve for migration directory path

--- tests/api/v1/migrations/get.test.js
+++ tests/api/v1/migrations/get.test.js
@@ -5,13 +5,17 @@ beforeAll(async () => {
   await orchestrator.cleanDatabase();
 });
 
-test("GET /api/v1/migrations returns 200", async () => {
-  const response = await fetch(`http://localhost:3000/api/v1/migrations`);
+describe("GET /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving pending migrations", async () => {
+      const response = await fetch(`http://localhost:3000/api/v1/migrations`);
 
-  expect(response.status).toBe(200);
+      expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(Array.isArray(responseBody)).toBe(true);
-  expect(responseBody.length).toBeGreaterThan(0);
+      expect(Array.isArray(responseBody)).toBe(true);
+      expect(responseBody.length).toBeGreaterThan(0);
+    });
+  });
 });
